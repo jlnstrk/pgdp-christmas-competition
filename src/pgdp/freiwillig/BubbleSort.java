e1bcd0fc2ef61c72c49570149d52b614664ca549
Erik Kynast <erikkynast@mytum.de>
2019-12-16 10:10:12 +0100

    Exercise-Template pushed by Artemis

---
+++ src/pgdp/freiwillig/BubbleSort.java
@@ -0,0 +1,11 @@
+package pgdp.freiwillig;
+
+import java.util.*;
+
+public class BubbleSort {
+
+    public void performSort(List<Date> input) {
+
+        //TODO: implement
+    }
+}
---
+++ src/pgdp/freiwillig/MergeSort.java
@@ -0,0 +1,12 @@
+package pgdp.freiwillig;
+
+import java.util.*;
+
+public class MergeSort {
+
+    public void performSort(List<Date> input) {
+
+        //TODO: implement
+    }
+
+}
