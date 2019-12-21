189e3f3a00bb867b47edb837483d6a1a7014182d
Erik Kynast <kynast@in.tum.de>
2019-12-21 14:49:21 +0100

    Added Template

---
+++ src/pgdp/freiwillig/Database.java
@@ -0,0 +1,12 @@
+package pgdp.freiwillig;
+
+// TODO Imports
+
+public class Database {
+    // TODO have fun :)  
+
+    public long getAverageQuantityPerMarketSegment(String marketsegment) {
+        // TODO
+	return 0;
+    }
+}
