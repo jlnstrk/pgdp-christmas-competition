bdc99a4763ff8e69f53598f40bd3e75173f740b9
Julian Ostarek <julianostarek@gmail.com>
2020-01-09 18:40:21 +0100

    Rocket Chat says: Parse the table files at once for multiple calls

--- src/pgdp/freiwillig/Database.java
+++ src/pgdp/freiwillig/Database.java
@@ -6,11 +6,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Database {
     private static File TBL_CUSTOMER = null, TBL_LINE_ITEM = null, TBL_ORDERS = null;
+    private Map<String, Set<Long>> customers = new HashMap<>();
+    private Map<Long, Set<Long>> orders = new HashMap<>();
+    private Map<Long, Set<Long>> lineItems = new HashMap<>();
 
     public static void setBaseDataDirectory(Path baseDirectory) {
         TBL_CUSTOMER = new File(baseDirectory.toString() +
@@ -21,69 +26,90 @@ public class Database {
                 File.separator + "orders.tbl");
     }
 
-    public long getAverageQuantityPerMarketSegment(String marketsegment) {
-        long averageQuantity = -1;
-        BufferedReader customerReader = null, ordersReader = null, lineItemReader = null;
-        try {
-            customerReader = new BufferedReader(new FileReader(TBL_CUSTOMER));
-            ordersReader = new BufferedReader(new FileReader(TBL_ORDERS));
-            lineItemReader = new BufferedReader(new FileReader(TBL_LINE_ITEM));
-            Set<Long> custKeys = new HashSet<>();
-            String l1;
-            while ((l1 = customerReader.readLine()) != null) {
-                if (l1.contains(marketsegment)) {
-                    long custKey = Long.parseUnsignedLong(l1, 0, l1.indexOf("|"), 10);
-                    custKeys.add(custKey);
-                }
-            }
+    public Database() {
+        parseCustomerData();
+        parseOrdersData();
+        parseLineItemsData();
+    }
 
-            Set<Long> orderKeys = new HashSet<>();
-            String l2;
-            while ((l2 = ordersReader.readLine()) != null) {
-                int custKeyFirst = l2.indexOf("|") + 1;
-                int custKeyLast = l2.indexOf("|", custKeyFirst) - 1;
-                long custKey = Long.parseUnsignedLong(l2, custKeyFirst, custKeyLast + 1, 10);
-                if (custKeys.contains(custKey)) {
-                    long orderKey = Long.parseUnsignedLong(l2, 0, custKeyFirst - 1, 10);
-                    orderKeys.add(orderKey);
+    private void parseCustomerData() {
+        try (FileReader tblReader = new FileReader(TBL_CUSTOMER)) {
+            try (BufferedReader customerReader = new BufferedReader(tblReader)) {
+                String l1;
+                while ((l1 = customerReader.readLine()) != null) {
+                    int segmentSepEnd = l1.lastIndexOf('|', l1.length() - 2);
+                    int segmentSepStart = l1.lastIndexOf('|', segmentSepEnd - 1);
+                    long custKey = Long.parseUnsignedLong(l1, 0, l1.indexOf("|"), 10);
+                    String key = l1.substring(segmentSepStart + 1, segmentSepEnd);
+                    customers.computeIfAbsent(key, k -> new HashSet<>())
+                            .add(custKey);
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            int orders = 0;
-            long totalQuant = 0;
-            String l3;
-            long currKey = 0;
-            while ((l3 = lineItemReader.readLine()) != null) {
-                int orderKeyLast = l3.indexOf("|") - 1;
-                long orderKey = Long.parseUnsignedLong(l3, 0, orderKeyLast + 1, 10);
-                if (currKey == orderKey || orderKeys.contains(orderKey)) {
-                    currKey = orderKey;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseOrdersData() {
+        try (FileReader tblReader = new FileReader(TBL_ORDERS)) {
+            try (BufferedReader ordersReader = new BufferedReader(tblReader)) {
+                String l2;
+                while ((l2 = ordersReader.readLine()) != null) {
+                    int custKeyFirst = l2.indexOf("|") + 1;
+                    int custKeyLast = l2.indexOf("|", custKeyFirst) - 1;
+                    long custKey = Long.parseUnsignedLong(l2, custKeyFirst, custKeyLast + 1, 10);
+                    long orderKey = Long.parseUnsignedLong(l2, 0, custKeyFirst - 1, 10);
+                    orders.computeIfAbsent(custKey, k -> new HashSet<>())
+                            .add(orderKey);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseLineItemsData() {
+        try (FileReader tblReader = new FileReader(TBL_LINE_ITEM)) {
+            try (BufferedReader lineItemReader = new BufferedReader(tblReader)) {
+                String l3;
+                while ((l3 = lineItemReader.readLine()) != null) {
+                    int orderKeyLast = l3.indexOf("|") - 1;
+                    long orderKey = Long.parseUnsignedLong(l3, 0, orderKeyLast + 1, 10);
                     int sepFront = ordinalIndexOf(l3, "|", 4, orderKeyLast + 1);
                     int sepBack = l3.indexOf("|", sepFront + 1);
                     long quantity = 100 * Long.parseUnsignedLong(l3, sepFront + 1, sepBack, 10);
-                    orders++;
-                    totalQuant += quantity;
+                    lineItems.computeIfAbsent(orderKey, k -> new HashSet<>())
+                            .add(quantity);
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            averageQuantity = Math.round((double) totalQuant / orders);
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
-            // ignore
-        } finally {
-            try {
-                if (customerReader != null) {
-                    customerReader.close();
+        }
+    }
+
+    public long getAverageQuantityPerMarketSegment(String marketsegment) {
+        long lineItemsCount = 0;
+        long totalQuantity = 0;
+        Set<Long> sgmtCustomers = customers.get(marketsegment);
+        for (Long custKey : sgmtCustomers) {
+            Set<Long> orderKeys = orders.get(custKey);
+            if (orderKeys != null) {
+                for (Long orderKey : orderKeys) {
+                    Set<Long> quantities = lineItems.get(orderKey);
+                    lineItemsCount += quantities.size();
+                    for (Long quantity : quantities) {
+                        totalQuantity += quantity;
+                    }
                 }
-                if (ordersReader != null) {
-                    ordersReader.close();
-                }
-                if (lineItemReader != null) {
-                    lineItemReader.close();
-                }
-            } catch (IOException e) {
-                // ignore
             }
         }
-        return averageQuantity;
+        return totalQuantity / lineItemsCount;
     }
 
     public static void main(String[] args) {
